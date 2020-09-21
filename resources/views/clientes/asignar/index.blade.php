f24816b7a28713e765c6ecfb937de91f03789967
MarcoAMtz99 <marcoamtz99@gmail.com>
2020-09-20 21:00:34 -0500

    Arreglando ERROR EN ASIGNAR CLIENTE POR QUITAR FOREACH

--- resources/views/clientes/asignar/index.blade.php
+++ resources/views/clientes/asignar/index.blade.php
@@ -8,7 +8,6 @@
 				<div class="row">
 					<div class="col-sm-4">
 						<h4>Vendedores:</h4>
-						<h2>{{echo count($vendedores)}}</h2>
 					</div>
 				</div>
 			</div>
@@ -22,7 +21,6 @@
 							<div class="col-sm-12">
 								
 								@if(count($vendedores) > 0)
-
 									<table id="tabla-vendedores" class="table table-stripped table-bordered table-hover" style="margin-bottom: 0px;">
 										<thead>
 											<tr class="info">
@@ -33,15 +31,20 @@
 											</tr>
 										</thead>
 										<tbody>
+									@foreach($laborales as $nombres)
 										@foreach($vendedores as $vendedor)
 											<tr>
+	
+											@if($nombres->id == $vendedor->empleado_id)
 												<td>{{$nombres->nombre}} {{$nombres->appaterno}} {{$nombres->apmaterno}}</td>	
 												<td>{{ $vendedor->grupo_id }}</td>
 												<td>{{ $vendedor->status }}</td>
 												<td class="text-center">
 													<input type="radio" name="vendedor_id" value="{{ $vendedor->id }}" required="">
-												</td>	
+												</td>
+												@endif
 											</tr>
+											@endforeach
 										@endforeach
 										</tbody>
 									</table>
