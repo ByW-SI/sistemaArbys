4c70e64b448394771c203de3e18a6bfd782585b5
MarcoAMtz99 <marcoamtz99@gmail.com>
2020-10-12 02:31:28 -0500

    prueba de vista para usuario diferente a admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -135,7 +135,7 @@
                                 </li>
                                 @endif
                                 @endforeach
-                                @if( Auth::user()->id == 1 || Auth::user()->id == 2 ||
+                                @if( Auth::user()->id == 1 || Auth::user()->id <99 ||
                                 Auth::user()->perfil->componentes()->where('nombre','asignar cliente')->first() )
                                 <li>
                                     <a href="#"
